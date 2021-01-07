c15c86701ecd92156f52393339f54ea4f12a69b8
Christopher House <chhouse@microsoft.com>
2021-01-07 11:13:01 -0600

    added models

---
+++ DurableFunctionsDemo/Models/SalesAggregateInput.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace DurableFunctionsDemo.Models
+{
+    public class SalesAggregateInput
+    {
+        [JsonProperty("regions")]
+        public IEnumerable<string> Regions { get; set; }
+        
+        [JsonProperty("divisions")]
+        public IEnumerable<string> Divisions { get; set; }
+    }
+}
---
+++ DurableFunctionsDemo/Models/SalesDataItem.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DurableFunctionsDemo.Models
+{
+    public class SalesDataItem
+    {
+        public int SalesDataId { get; set; }
+        
+        public string Region { get; set; }
+        
+        public string Division { get; set; }
+        
+        public Guid CustomerId { get; set; }
+        
+        public DateTime TransactionDate { get; set; }
+        
+        public decimal TransactionAmount { get; set; }
+    }
+}
