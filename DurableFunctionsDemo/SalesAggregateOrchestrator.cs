9641e851e8dbe207ed25ed857f8513a956df0582
Christopher House <chhouse@microsoft.com>
2021-01-12 09:24:48 -0600

    changes to orch

--- DurableFunctionsDemo/SalesAggregateOrchestrator.cs
+++ DurableFunctionsDemo/SalesAggregateOrchestrator.cs
@@ -1,10 +1,15 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using DurableFunctionsDemo.Models;
+using Microsoft.Azure.Storage.Blob;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.DurableTask;
+using Microsoft.EntityFrameworkCore.Internal;
 using Newtonsoft.Json;
 
 namespace DurableFunctionsDemo
@@ -14,7 +19,8 @@ namespace DurableFunctionsDemo
         [FunctionName("SalesAggregateOrchestrator")]
         public static async Task<SalesAggregateInput> RunOrchestrator([OrchestrationTrigger] IDurableOrchestrationContext context, 
             [DurableClient] IDurableOrchestrationClient durableOrchestrationClient,
-            [Blob("input/cf-input.json", FileAccess.Read, Connection = "input-storage-connection-string")] string regionsAndDivisions)
+            [Blob("input/cf-input.json", FileAccess.Read, Connection = "input-storage-connection-string")] string regionsAndDivisions,
+            Binder binder)
         {
             IList<Task<IEnumerable<GetSalesDataOutput>>> tasks = new List<Task<IEnumerable<GetSalesDataOutput>>>();
             
@@ -35,6 +41,26 @@ namespace DurableFunctionsDemo
             }
             
             var results = await Task.WhenAll(tasks);
+
+            if (results.Any())
+            {
+                var notEmptyResults = results.Where(result => result != null)
+                    .SelectMany(result => result);
+                
+                var jsonResults = JsonConvert.SerializeObject(notEmptyResults);
+                var jsonBytes = Encoding.UTF8.GetBytes(jsonResults);
+
+                // Previously we were getting several zero byte blobs per run.  Switching to runtime binding seems to have solved that
+                var attributes = new Attribute[]
+                {
+                    new BlobAttribute("output/{rand-guid}.json", FileAccess.Write),
+                    new StorageAccountAttribute("input-storage-connection-string")
+                };
+
+                await using var outputStream = await binder.BindAsync<Stream>(attributes);
+                await outputStream.WriteAsync(jsonBytes, 0, jsonBytes.Length);
+                await outputStream.FlushAsync();
+            }
             
             return inputs;
         }
