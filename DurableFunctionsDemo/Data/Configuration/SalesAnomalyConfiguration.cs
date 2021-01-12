57803753b2f0eeb2b0218c03674613f44e2db0e6
Christopher House <chhouse@microsoft.com>
2021-01-12 09:36:01 -0600

    config for anomaly data

--- DurableFunctionsDemo/Data/Configuration/SalesAnomalyConfiguration.cs
+++ DurableFunctionsDemo/Data/Configuration/SalesAnomalyConfiguration.cs
@@ -28,32 +28,4 @@ namespace DurableFunctionsDemo.Data.Configuration
                 .IsRequired();
         }
     }
-    
-    public class SalesDataItemConfiguration : IEntityTypeConfiguration<SalesDataItem>
-    {
-        public void Configure(EntityTypeBuilder<SalesDataItem> builder)
-        {
-            builder.ToTable("SalesData", "dbo");
-            
-            builder.HasKey(table => table.SalesDataId);
-            
-            builder.Property(table => table.Region)
-                .HasMaxLength(10)
-                .IsRequired();
-
-            builder.Property(table => table.Division)
-                .HasMaxLength(10)
-                .IsRequired();
-
-            builder.Property(table => table.CustomerId)
-                .IsRequired();
-
-            builder.Property(table => table.TransactionDate)
-                .IsRequired();
-
-            builder.Property(table => table.TransactionAmount)
-                .HasColumnType("decimal(18,4)")
-                .IsRequired();
-        }
-    }
 }
---
+++ DurableFunctionsDemo/Models/SalesAnomaly.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DurableFunctionsDemo.Models
+{
+    public class SalesAnomaly
+    {
+        public int SalesAnomalyId { get; set; }
+
+        public string RunIdentifier { get; set; }
+
+        public string Region { get; set; }
+
+        public string Division { get; set; }
+
+        public Guid CustomerId { get; set; }
+
+        public decimal AnomalyCaclulationResult { get; set; }
+    }
+}
